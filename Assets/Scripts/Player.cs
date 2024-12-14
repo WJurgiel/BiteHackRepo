8c14698a90ee3903922dd89a9b6cd8c3111e7232
WJurgiel <wjurgiel@student.agh.edu.pl>
2024-12-14 14:40:03 +0100

    Events of statistics changing added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,10 @@ public class Player : MonoBehaviour
     [SerializeField] private StatsSO stats;
     [SerializeField] private PlayerStatsSO playerStats;
 
+    
     private int _health;
+    
+    
     void Start()
     {
             _health = stats.health;
@@ -17,6 +20,27 @@ public class Player : MonoBehaviour
         {
             stats.GetDamage(Random.Range(0, stats.damage));
             Debug.Log("Private health: " + _health + " SO health = " + stats.health) ;
+        }
+        _testing_EventTests();
+    }
+
+    void _testing_EventTests()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            stats.HealYourself(20);
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            stats.IncreaseSpeed(2);
+        }
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            stats.IncreaseDamage(2);
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            stats.IncreaseDefense(2);
         }
     }
 }
--- Assets/Scripts/ScriptableObjects/StatsSO.cs
+++ Assets/Scripts/ScriptableObjects/StatsSO.cs
@@ -10,15 +10,51 @@ public class StatsSO : ScriptableObject
     public int speed;
     public int defense;
     public int damage;
+    //Call this to get know if the player or enemy is damaged
     [System.NonSerialized] public UnityEvent<int> getDamageEvent = new UnityEvent<int>();
+    [System.NonSerialized] public UnityEvent<int> healhYourselfEvent = new UnityEvent<int>();
+    [System.NonSerialized] public UnityEvent<int> healYourselfFullEvent = new UnityEvent<int>();
+    [System.NonSerialized] public UnityEvent<int> increaseSpeedEvent = new UnityEvent<int>();
+    [System.NonSerialized] public UnityEvent<int> increaseDefenseEvent = new UnityEvent<int>();
+    [System.NonSerialized] public UnityEvent<int> increaseDamageEvent = new UnityEvent<int>();
     private void OnEnable()
     {
         health = maxHealth;
     }
-
+    
     public void GetDamage(int damage)
     {
+        Debug.Log("event eventuje");
         health -= damage;
         getDamageEvent.Invoke(health);
+    }
+
+    public void HealYourself(int amount)
+    {
+        health += amount;
+        healhYourselfEvent.Invoke(health);
+    }
+
+    public void HealYourselfFull()
+    {
+        health = maxHealth;
+        healYourselfFullEvent.Invoke(0); // hardcoded
+    }
+    public void IncreaseSpeed(int toAdd)
+    {
+        speed += toAdd;
+        increaseSpeedEvent.Invoke(speed);
+    }
+
+    public void IncreaseDefense(int toAdd)
+    {
+        defense += toAdd;
+        increaseDefenseEvent.Invoke(defense);
+    }
+
+    public void IncreaseDamage(int toAdd)
+    {
+        damage += toAdd;
+        increaseDamageEvent.Invoke(damage);
     }
 }

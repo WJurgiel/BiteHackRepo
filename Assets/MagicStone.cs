fb115b41d48a799a00459ea9cfc16f27aba4b7fc
WJurgiel <wjurgiel@student.agh.edu.pl>
2024-12-15 08:34:50 +0100

    Hopefully the last commit today
    bomboclat, I think we made it

---
+++ Assets/MagicStone.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class MagicStone : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private Light2D light;
+    [SerializeField] private Sprite[] stoneSprites;
+    [SerializeField] private BoneSO bone;
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        bone.e_Pickup.AddListener(CheckBonesCount);
+        light = GetComponentInChildren<Light2D>();
+    }
+    void CheckBonesCount()
+    {
+        if (bone.boneCurrent < 10 && sr.sprite != stoneSprites[0])
+        {
+            sr.sprite = stoneSprites[0];
+            light.intensity = 0.0f;
+        }
+        else if (bone.boneCurrent >= 10 && sr.sprite != stoneSprites[1])
+        {
+            sr.sprite = stoneSprites[1];
+            light.intensity = 5f;
+        }
+        else if (bone.boneCurrent >= 20 && sr.sprite != stoneSprites[2])
+        {
+            sr.sprite = stoneSprites[2];
+            light.intensity = 10f;
+        }
+    }
+}

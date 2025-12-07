0c14f336ab009e7bd9f1cf2099854e17c57995c5
Ania <ajasielec@student.agh.edu.pl>
2025-12-07 18:59:20 +0100

    UI updates
    add new hourglass, add option panel with volume settings

--- Assets/Scripts/UI/ButtonFunctions.cs
+++ Assets/Scripts/UI/ButtonFunctions.cs
@@ -1,6 +1,7 @@
 using ScriptableObjects;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace UI
 {
@@ -8,28 +9,58 @@ namespace UI
     {
         [SerializeField]
         private BoneSo bone;
+
+        [SerializeField]
+        private GameObject optionsPanel;
+
+        [SerializeField]
+        private Slider volumeSlider;
+
+        [SerializeField]
+        private AudioSource menuMusic;
+
+        private void Start()
+        {
+            volumeSlider.value = 1;
+            volumeSlider.onValueChanged.AddListener(delegate {SetVolume ();});
+        }
+
         public void GoToMainMenu()
         {
             SceneManager.LoadScene("MainMenu");
         }
+
         public void ContinueGame()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             bone.boneCurrent = 0;
         }
+
         public void NewGame()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             bone.boneCurrent = 0;
         }
+
         public void ShowSettings()
         {
-
+            optionsPanel.SetActive(true);
         }
+        public void CloseSettings()
+        {
+            optionsPanel.SetActive(false);
+        }
+
         public void QuitGame()
         {
             Application.Quit();
         }
 
+        public void SetVolume()
+        {
+            menuMusic.volume = volumeSlider.value;
+            PlayerPrefs.SetFloat("volume", volumeSlider.value);
+            PlayerPrefs.Save();
+        }
     }
 }
